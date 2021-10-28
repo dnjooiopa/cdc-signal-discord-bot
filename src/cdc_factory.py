57f37c02c1fd518b84023a24cc2c88c6099b2e40
Kittisak Phormraksa <dnjooiopa@gmail.com>
2021-10-28 11:36:01 +0700

    Remove keyword "pairs"

--- src/cdc_factory.py
+++ src/cdc_factory.py
@@ -174,7 +174,7 @@ def get_signals_with_tf(tf, dayOffset):
 
 def get_historical_signal(pairs):
   if pairs not in allPairs:
-    return f'❌ Pairs not exists : {pairs.upper()}\nType "!cdc add pair {pairs}" to add this pairs.'
+    return f'❌ Pairs not exists : {pairs.upper()}\nType "!cdc add {pairs}" to add this pairs.'
 
   msg = ''
   for tf in periods:
