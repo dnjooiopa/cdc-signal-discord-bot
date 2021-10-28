b39edbb30c55a53696d8c46a5155e10ae764001d
Kittisak Phormraksa <dnjooiopa@gmail.com>
2021-10-28 09:18:48 +0700

    Add info

--- src/cdc_signal_bot.py
+++ src/cdc_signal_bot.py
@@ -54,11 +54,13 @@ async def on_message(message):
       if contents[1] == 'update':
         refetch()
         msg = get_all_signals(dayOffset)
-      if contents[1] == 'future':
+      elif contents[1] == 'future':
         dayOffset = 1
         msg = get_all_signals(dayOffset)
-      if contents[1] == 'pairs' or contents[1] == 'list':
+      elif contents[1] == 'pairs' or contents[1] == 'list':
         msg = get_availabel_pairs()
+      elif contents[1] == 'info':
+        msg = 'รอก่อน กำลังทำ...'
     elif len(contents) == 3:
       if contents[1] == 'history':
         msg = get_historical_signal(contents[2])
